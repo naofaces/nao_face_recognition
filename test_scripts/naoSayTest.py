c4da43c6aafd89affed2ca952efcd92a82fd4d61
yphgit <yph.goo@gmail.com>
2016-11-03 18:53:03 +0100

    Added testscripts

---
+++ test_scripts/naoSayTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import naoIp
+naoqi_root = '/home/USERNAME/naoSoftware/pynaoqi/'
+sys.path.insert(0, naoqi_root)
+import naoqi
+from naoqi import ALProxy
+
+
+# sentence = "This was a triumph. I'm making a note here: Huge Success. It's hard to overstate my satisfaction. Aperture Science. We do what we must, because we can, For the good of all of us, except the ones who are dead."
+sentence = "lol"
+
+tts = ALProxy('ALTextToSpeech', naoIp.Ip, 9559)
+tts.say(sentence)
+
